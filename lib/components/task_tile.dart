809d91dd6e4f35079d93a2c0d10fb82fea328761
lou <dustyfingers01257@gmail.com>
2021-05-03 07:11:30 -0600

    learned about local state in flutter

--- lib/components/task_tile.dart
+++ lib/components/task_tile.dart
@@ -6,7 +6,28 @@ class TaskTile extends StatelessWidget {
     return ListTile(
       contentPadding: EdgeInsets.all(0),
       title: Text('This is a task.'),
-      trailing: Checkbox(value: false),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+class TaskCheckbox extends StatefulWidget {
+  @override
+  _TaskCheckboxState createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: isChecked,
+      onChanged: (bool checkboxState) {
+        setState(() {
+          isChecked = checkboxState;
+        });
+      },
     );
   }
 }
